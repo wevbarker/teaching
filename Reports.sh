721d4bbd075f755bf9d504cc8cb438c8d1d1f348
William Barker <wb263@cam.ac.uk>
2024-09-10 22:49:07 +0100

    With Seongjae report.

---
+++ Reports.sh
@@ -0,0 +1,216 @@
+#================================#
+#  All the reports from 2023-24  #
+#================================#
+
+Oscillations, waves and optics
+Quantum physics
+Condenesd matter physics
+
+#=========#
+#  Peter  #
+#=========#
+
+3*8
+
+"Peter has made a strong start to Physics A through the OWO course. He tends to be somewhat quiet in supervisions, but often arrives earlier than the others and manages to ask some pointed questions that have bothered him on the question sheet. His handwriting is sometimes questionable, but he can write well when he needs to. Together with his peers, he has expressed expressed some (justifiable) frustration with the poor quality of the Experimental Methods sub-course. I have communicated with the lecturer and obtained information about the textbook (Control Theory for Physicists, Bechhoefer) from which the problematic parts of this course were copied: I strongly recommend all the students to supplement the course notes with this book."
+
+3*8
+
+"Peter performed moderately well in the mock exam, though he was frustrated with the question about quantised mass acquisition on the 2D waveguide. I also believe that he is less comfortable with the more abstract concepts in quantum mechanics than he was with the last term's material. Possibly this is due to the way Tripos tries to teach *practical* quantum mechanics, such as might be useful for experimentalists, rather than providing a serious introduction to the mathematical axioms. Anyhow, he should do well in the exam."
+
+3*4
+
+"Peter clearly made good headway with revision during the Easter break, and propagated forwards quite a few questions from the quantum course. He seems frustrated with the lack of theoretical underpinning to the condensed matter course, but has found a way to navigate the large amount of material. I have recommended the book by Ashcroft and Mermin."
+
+#========#
+#  Matt  #
+#========#
+
+3*8
+
+"Matt has made a strong start to Physics A through the OWO course. He interacts very well during supervisions, and often engages in a problem-solving rapport with his peers. Together with his peers, he has expressed expressed some (justifiable) frustration with the poor quality of the Experimental Methods sub-course. I have communicated with the lecturer and obtained information about the textbook (Control Theory for Physicists, Bechhoefer) from which the problematic parts of this course were copied: I strongly recommend all the students to supplement the course notes with this book."
+
+3*8
+
+"Matt has continued to work well during Lent term on the quantum course. The only possible sticking point has been about how seriously to take the specific derivations of certain results in the course notes (e.g quantisation of the Hydrogen-like atom, enumerating the bound states of a square well as a function of depth and width). He is learning to understand principles (and so re-derive results himself) instead of memorising formulae, which is the main skill for the exam."
+
+3*4
+
+"Matt has made good progress with the condensed matter course, and has been able to answer most of the questions that were set. He has also been able to explain the concepts to me, which is a good sign. There may be some deliberate avoidance of the material on semiconductors, but this is usual in the A course (lots of material to cram before the exam, and the semiconductor topic is poorly written)."
+
+#===========#
+#  Michele  #
+#===========#
+
+3*8
+
+"Michele has made a strong start to Physics A through the OWO course. He interacts very well during supervisions, and often engages in a problem-solving rapport with her peers. He has a very good habit of presenting his results using a stylus on his tablet: his answers are always legible. Together with her peers, he has expressed expressed some (justifiable) frustration with the poor quality of the Experimental Methods sub-course. I have communicated with the lecturer and obtained information about the textbook (Control Theory for Physicists, Bechhoefer) from which the problematic parts of this course were copied: I strongly recommend all the students to supplement the course notes with this book."
+
+3*8
+
+"Michele has continued to work well during Lent term on the quantum course. My only advice is to not try to use physical intuition when learning QM for the first time: the important thing is to understand the mathematical framework in which QM is set. Later on, after you have solved many problems and considered the semi-classical limit and correspondence principle, you will find your intuition for QM develops on its own."
+
+3*4
+
+"Michele has made excellent progress with the condensed matter course. He has picked apart lots of the weak assumptions that underpin some results in the course, during discussions. His solutions are almost always correct. There may be some deliberate avoidance of the material on semiconductors, but this is usual in the A course (lots of material to cram before the exam, and the semiconductor topic is poorly done)."
+
+#========#
+#  Finn  #
+#========#
+
+3*8
+
+"Finn has made a strong start to Physics A through the OWO course. I'd encourage him to forget some of the rules of thumb that he may have picked up from Part IA, and re-learn oscillating systems from scratch in the context of this course. His written work is good, though sometimes he does not attempt all the questions. Together with his peers, he has expressed expressed some (justifiable) frustration with the poor quality of the Experimental Methods sub-course. I have communicated with the lecturer and obtained information about the textbook (Control Theory for Physicists, Bechhoefer) from which the problematic parts of this course were copied: I strongly recommend all the students to supplement the course notes with this book."
+
+3*8
+
+"Finn has continued to work well during Lent term on the quantum course. He has a good habit of asking questions about the material, and has been able to answer most of the questions that were set. He has also been able to explain the concepts to me, which is a good sign. I am still slightly concerned that there are gaps in his solutions where he does not attempt the question with true zeal. Usually we are able to go over these in the supervision, and I'd encourage Finn to use the Easter vacation to re-attempt these points with pen and paper."
+
+3*4
+
+"Finn has made fair progress with the condensed matter course, though there are still some quantum sticking points from last term that we have continued to discuss. His written solutions this term have been more complete than last term, which is a good sign."
+
+#=========#
+#  Sofia  #
+#=========#
+
+2*8
+
+"Sofia has made a strong start to Physics A through the OWO course. She asks many, many questions about points which cause her confusion. She almost always attempts all the questions, and clearly annotates her sticking points in the solutions. Together with her peers, she has expressed expressed some (justifiable) frustration with the poor quality of the Experimental Methods sub-course. I have communicated with the lecturer and obtained information about the textbook (Control Theory for Physicists, Bechhoefer) from which the problematic parts of this course were copied: I strongly recommend all the students to supplement the course notes with this book."
+
+2*8
+
+"Sofia has worked hard to understand the quantum course, and continues to ask many questions both in the supervision and in her problem sheet solutions. I suspect she lacks some confidence in QM due to the novelty of the framework, but she is making good progress."
+
+2*4
+1*1
+
+"Sofia has worked exceptionally hard both throughout the Easter vacation and during the condensed matter course. I get the impression that QM is her weakest point, and lots of time this term has been spent filling in the gaps in her knowledge. She has made decent progress with the condensed matter course, and has been able to answer most of the questions that were set. We have arranged a few extra 1:1 supervisions to go over a final list of questions that she has prepared."
+
+
+#===========#
+#  William  #
+#===========#
+
+2*8
+
+"William has made a very strong start to Physics A through the OWO course. He can be quite reserved during supervisions unless the group size is small, but under the right conditions he engages in keen discussion about the physics. In fact, he is a remarkably strong physicist. He has a very good habit of scanning written work and supplementing it with elaborate numerical scripts (mostly python): his answers are immaculate. Together with his peers, he has expressed expressed some (justifiable) frustration with the poor quality of the Experimental Methods sub-course. I have communicated with the lecturer and obtained information about the textbook (Control Theory for Physicists, Bechhoefer) from which the problematic parts of this course were copied: I strongly recommend all the students to supplement the course notes with this book."
+
+2*8
+
+"William has continued to perform extremely well during Lent term on the quantum course. His written work and interactions during supervisions suggest that he is on track for a high first."
+
+2*4
+
+"As with the quantum course, William has made excellent progress with the condensed matter course. He may well not be entirely satisfied with the very qualitative nature of this course, but he will have no difficulty in memorising what is needed (complete with questionable assumptions made in the notes). He is still on track for a high first."
+
+#========#
+#  Yang  #
+#========#
+
+3*8
+
+"Yang has made a robust start to Physics A through the OWO course. Though he engages well in supervisions, his written work is occasionally incomplete (or absent). Together with his peers, he has expressed expressed some (justifiable) frustration with the poor quality of the Experimental Methods sub-course. I have communicated with the lecturer and obtained information about the textbook (Control Theory for Physicists, Bechhoefer) from which the problematic parts of this course were copied: I strongly recommend all the students to supplement the course notes with this book."
+
+3*8
+
+"Yang did not perform as well as I had hoped in the mock exam. It was not at all clear that he revised the Michaelmas material during the vacation, and he seemed to loose heart after attempting the first couple of questions. By contrast, he was very apologetic about this in correspondence, and has since made a strong effort to catch up throughout the QM course. With focus and effort, he should do well in the exam."
+
+3*4
+
+"Yang has made moderate progress with the condensed matter course, though his zeal seems to have dropped off somewhat since Lent term. His written solutions are often incomplete, or not submitted. However, during discussions about the theoretical underpinning of some of the CM course, he has shown a good understanding of how to apply the principles of QM to the condensed matter context (e.g. Bloch waves). He should still succeed in the exam, though he must be careful to revise the material thoroughly."
+
+#=========#
+#  Allen  #
+#=========#
+
+3*8
+
+"Following an unusually large number of 1:1 interactions with Allen after supervisions and via email, I am led to believe that there is a very good chance he will become a professional theoretician. Someone like this comes along once in every ~35% of cohorts. Allen has a non-standard trajectory into the NST tripos (which I do not fully understand, via some exchange programme), and is highly dissatisfied with the experimental/engineering orientation of Part IB. He is very, very able as a mathematician, and has a strong desire to do theory. He must (really must) learn to jump through the hoop of part IB so that he can enjoy Part II."
+
+3*8
+
+"Allen has taken my advice to heart about getting through the IB exams. His written work is not always complete, but I am more concerned with encouraging him to do as many past papers as possible before exam day (I know he can solve the problems on the question sheet, but can he present his results in the Tripos style on the exam?). We continue to enjoy more broad discussions about theoretical physics, and I have recommended some books to him that he might enjoy."
+
+3*4
+
+"As with Lent term, Allen has continued to apply himself to Tripos. I am not able to supplement his knowledge in the CM course with more advanced material, since this is not my area, but he is very good at researching material himself. At this stage I do not foresee any problems with Allen for the exam, so long as he continues to practice papers. We have a plan to collaborate on a paper in Cambridge during the summer vacation, and I hope to help him with a reference during the next academic year."
+
+#============#
+#  Xiangwei  #
+#============#
+
+1*4
+
+"Xiangwei has made a heroic start to his project. He was hospitalised during the early weeks of Michaelmas term, and attended the project advertisement meeting remotely from Addenbrookes. He has continued to meet remotely during term-time. Good progress has been made on the literature review, and a convincing project plan has been drawn up. I do not foresee any problems with this project."
+
+1*4
+
+"Xiangwei has continued to make good progress with his project. He has developed a growing body of TeX which should easily be reformed into a substantive report later in the year. He has also gotten to grips with Mathematica, and used it to meet several milestones set out in the project plan. I foresee no problems with the project."
+
+1*4
+
+"Xiangwei prepared a very high-quality report which contained novel and interesting results. He delivered an interesting in-person talk to our group, despite ongoing health problems. He performed very robustly during the viva. The work probably requires more thinking before something publishable can be extracted, but this is more a function of our hasty formulation of the project abstract than any fault of Xiangwei's."
+
+#=======#
+#  Ray  #
+#=======#
+
+1*4
+
+"Ray has made a very strong start to his project, he frequently works alongside another of the group's students who is working on a similar topic. He has made good progress on the literature review. I foresee no problems with this project." 
+
+1*4
+
+"Ray has continued to make excellent progress with his project. He has used the xAct software suite to obtain the field equations that he needs in record time (this kind of task has taken previous students over a year) and is now looking for novel exact solutions. I foresee no problems with the project."
+
+1*4
+
+"Ray prepared a very high-quality report which contained novel and interesting results. He delivered an interesting in-person talk to our group. He performed very robustly during the viva. He has discovered what looks to be two distinct novel exact solutions the aether-scalar-tensor (AeST) field equations, which I hope will be published during the summer."
+
+#============#
+#  Benjamin  #
+#============#
+
+1*4
+
+"Benjamin has made a strong start to his project. He has made good progress on the literature review. He has also completed several first-step calculations under guidance, with excellent results. I foresee no problems with this project."
+
+1*4
+
+"Benjamin has continued to make excellent progress with his project. He has begun to build a robust Mathematica system to numerically investigate the parameter space of the model. He has also begun to TeX his results. I foresee no problems with the project."
+
+1*4
+
+"Benjamin prepared a very high-quality report which contained novel and interesting results. He delivered an interesting in-person talk to our group. He performed very robustly during the viva. The work will certainly be published, with his continued interest, and he has been nominated for the theory prize after a consult with the co-assessor."
+
+#=======#
+#  Tom  #
+#=======#
+
+1*4
+
+"Tom has made a strong start to his project. He has made good progress on the literature review. He has also completed several first-step calculations under guidance, with excellent results. I foresee no problems with this project."
+
+1*4
+
+"Tom has continued to make excellent progress with his project. He has successfully obtained the modified Friedmann equations of quadratic metric-affine gravity using the xAct software: this is publishable if it can be packed into a neat paper, even with some numerical solutions. He has also begun to TeX his results. I foresee no problems with the project."
+
+1*4
+
+"Tom prepared a very high-quality report which contained many novel and interesting results. He delivered an interesting in-person talk to our group, and was also invited to speak at a collaborator's group in a different institution. He performed very robustly during the viva. The work will certainly be published, with his continued interest."
+
+Seongjae
+
+1*4
+
+"Seongjae has made a strong start to his project. He has made fair progress on the literature review, and is also taking the supplemental course on canonical gravity from Part III maths. During meetings, he has asked probing questions about the Dirac--Bergmann Hamiltonian constraint algorithm, and we have discussed this at length. He is yet to thoroughly apply the method to classical electromagnetism, but I believe he understands the principles. I foresee no problems with this project."
+
+1*4
+
+"Seongjae has made moderate progress this term. We have decided to pursue the analysis of the teleparallel propagator structure, instead of the Dirac--Bergmann method. Seongjae spend a good deal of time linearising the gravitational action, and together we used the PSALTer software to compute the propagator. He has also begun to TeX his results. Seongjae will need to work hard over Easter to ensure that both the results and the write-up are of high quality. I believe there are some personal mitigating factors in Seongjae's workflow, and he tells me that he has been in contact with his tutor about this."
+
+1*4
+
+"Seongjae experienced a challenging end to his project. Some personal factors became very distracting during and after the vacation, and this was compounded by the need to prepare for the written exams. Once again, Seongjae has been in contacty with his college to discuss these issues. Seongjae was able to obtain some novel results (a way to alleviate the strong coupling of a particular kind of modified teleparallel theory), however the quality of the final report was shaky due to severe time pressures, despite the fact that he secured an extension. Seongjae's group presentation and viva were similarly shaky."
+
